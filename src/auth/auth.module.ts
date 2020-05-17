e63b21eab6f72c52ab21167de9d4b538cb4417ee
Kammradt <vinicius.kammradt1@gmail.com>
2020-05-17 18:23:41 -0300

    :sparkles: Adding JwtStrategy to run before each request and validate token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,25 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import * as config from 'config'
+
+const { expiresIn, secret } = config.get('jwt');
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || secret,
+      signOptions: { expiresIn: 3600 },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {
 }
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from '../user.repository';
+import { User } from '../user.entity';
+import * as config from 'config';
+import { JwtPayload } from '../dto/jwt-payload';
+
+const { secret } = config.get('jwt');
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || secret,
+    });
+  }
+
+  // Run after decoding the JWT into a JwtPayload.
+  // We implement the way we want to validate, and what is returned is injected into our request
+  async validate(payload: JwtPayload): Promise<User> {
+    return await this.userRepository.findByEmail(payload.username);
+  }
+}
