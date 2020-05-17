8a0d1fee31b34043aab1ad1b8a32caa7b0a18748
Kammradt <vinicius.kammradt1@gmail.com>
2020-05-17 16:56:10 -0300

    :sparkles: :construction: Adding temporary handler for username duplicates

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,13 +2,18 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { RegisterRequest } from './dto/register-request';
 import { UserRole } from './enum/user-role.enum';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { LoginRequest } from './dto/login-request';
 import * as bcrypt from 'bcryptjs';
 
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
-  public async register(registerRequest: RegisterRequest): Promise<User> {
+  public async register(registerRequest: RegisterRequest): Promise<void> {
     const { email, password } = registerRequest;
 
     const user = this.create();
@@ -16,7 +21,15 @@ export class UserRepository extends Repository<User> {
     user.role = UserRole.REGULAR;
     user.salt = await bcrypt.genSalt();
     user.password = await this.hashPassword(password, user.salt);
-    await user.save();
+
+    try {
+      await user.save();
+    } catch (e) {
+      if (e.code === '23505') { // TODO create a ExceptionGuard
+        throw new ConflictException('Username not available');
+      } else {
+        throw new InternalServerErrorException();
+      }
 
     return user;
   }
