a11fc4e10f986c5301b8d0178763e01f6d3fa090
Kammradt <vinicius.kammradt1@gmail.com>
2020-05-17 15:37:17 -0300

    :wrench: Adding global DTOs configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { ClassSerializerInterceptor, INestApplication, ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+function addDTOsConfiguration(app: INestApplication) {
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+  }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+}
+
+async function start() {
   const serverConfig = config.get('server');
   const port = process.env.PORT || serverConfig.port;
-
   const app = await NestFactory.create(AppModule);
+
+  addDTOsConfiguration(app);
+
   await app.listen(port);
 }
 
-bootstrap();
+start();
