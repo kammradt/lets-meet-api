2b1eaecab42d126b7554b718ccf9a7c017f16968
Kammradt <vinicius.kammradt1@gmail.com>
2020-05-18 20:19:14 -0300

    :art: :recycle: Splitting up UserModule into AuthModule

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { forwardRef, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtResponse } from './dtos/jwt-response';
+import { LoginRequest } from './dtos/login-request';
+import { UserService } from '../users/user.service';
+import { JwtPayload } from './dtos/jwt-payload';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UserService))
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {
+  }
+
+  public async login(loginRequest: LoginRequest): Promise<JwtResponse> {
+    const { email, password } = loginRequest;
+    const user = await this.userService.findByEmail(email);
+
+    if (!await user.hasCorrectPassword(password)) {
+      throw new UnauthorizedException();
+    }
+
+    const payload: JwtPayload = { email };
+    const token: string = await this.jwtService.sign(payload);
+    return { token };
+  }
+}
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,12 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
-import { RegisterRequest } from './dto/register-request';
-import { LoginRequest } from './dto/login-request';
-import { JwtService } from '@nestjs/jwt';
-import { JwtResponse } from './dto/jwt-response';
-import { JwtPayload } from './dto/jwt-payload';
-import { UserRole } from './enum/user-role.enum';
+import { RegisterRequest } from './dtos/register-request';
+import { UserRole } from './user-role.enum';
 import { User } from './user.entity';
 import * as bcrypt from 'bcryptjs';
 
@@ -14,8 +10,7 @@ import * as bcrypt from 'bcryptjs';
 export class UserService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository,
-    private jwtService: JwtService) {
+    private userRepository: UserRepository) {
   }
 
   public async register(registerRequest: RegisterRequest): Promise<void> {
@@ -30,30 +25,12 @@ export class UserService {
     return this.userRepository.persist(user);
   }
 
-  public async login(loginRequest: LoginRequest): Promise<JwtResponse> {
-    const email: string = await this.validateCredentials(loginRequest);
-    const payload: JwtPayload = { email };
-    const token: string = await this.jwtService.sign(payload);
-    return { token };
+  public findByEmail(email: string): Promise<User> {
+    return this.userRepository.findByEmail(email);
   }
 
   private async hashPassword(password: string, salt: string): Promise<string> {
     return await bcrypt.hash(password, salt);
-  }
-
-  private async validateCredentials(loginRequest: LoginRequest): Promise<string> {
-    const { email, password } = loginRequest;
-    const user = await this.findByEmail(email);
-
-    if (!await user.hasCorrectPassword(password)) {
-      throw new UnauthorizedException();
-    }
-
-    return user.email;
-  }
-
-  private findByEmail(email: string): Promise<User> {
-    return this.userRepository.findByEmail(email);
   }
 
 }
