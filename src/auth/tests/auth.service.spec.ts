0057eda0bcdb35376d34dc424093030029da7881
Kammradt <vinicius.kammradt1@gmail.com>
2020-05-18 22:25:28 -0300

    :white_check_mark: Adding tests to UserService

--- src/auth/tests/auth.service.spec.ts
+++ src/auth/tests/auth.service.spec.ts
@@ -1,18 +1,73 @@
+import { LoginRequest } from '../dtos/login-request';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from '../auth.service';
+import { UserService } from '../../users/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../../users/user.entity';
+import { JwtResponse } from '../dtos/jwt-response';
+import { UnauthorizedException } from '@nestjs/common';
+import mock = jest.mock;
+
+
+const mockUserService = () => ({
+  findByEmail: jest.fn(),
+});
+
+const mockJwtService = () => ({
+  sign: jest.fn(),
+});
+
+const mockLoginRequest: LoginRequest = {
+  email: 'email@test.com',
+  password: '12345678',
+};
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService;
+  let userService;
+  let jwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UserService, useFactory: mockUserService },
+        { provide: JwtService, useFactory: mockJwtService },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = await module.get<AuthService>(AuthService);
+    userService = await module.get<UserService>(UserService);
+    jwtService = await module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('login', () => {
+    let user;
+
+    beforeEach(() => {
+      user = new User();
+      user.email = 'email@test.com';
+      user.hasCorrectPassword = jest.fn();
+    });
+
+    it('should return a JwtResponse', async () => {
+      expect(userService.findByEmail).not.toHaveBeenCalled();
+      expect(user.hasCorrectPassword).not.toHaveBeenCalled();
+      expect(jwtService.sign).not.toHaveBeenCalled();
+
+      userService.findByEmail.mockResolvedValue(user);
+      user.hasCorrectPassword.mockResolvedValue(true);
+      jwtService.sign.mockResolvedValue('123token123')
+
+      const jwtResponse: JwtResponse = await authService.login(mockLoginRequest)
+      expect(jwtResponse.token).toBe('123token123')
+    });
+
+    it('should throw an UnauthorizedException', () => {
+      userService.findByEmail.mockResolvedValue(user);
+      user.hasCorrectPassword.mockResolvedValue(false);
+
+      expect(authService.login(mockLoginRequest)).rejects.toThrow(UnauthorizedException)
+    });
   });
 });
