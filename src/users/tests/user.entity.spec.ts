e6fdd005cca52e7d2a0c5f405fe45fd8edd85ee6
Kammradt <vinicius.kammradt1@gmail.com>
2020-05-18 23:02:27 -0300

    :white_check_mark: Adding tests to User Entity

---
+++ src/users/tests/user.entity.spec.ts
@@ -0,0 +1,35 @@
+import * as bcrypt from 'bcryptjs';
+import { User } from '../user.entity';
+
+describe('User entity', () => {
+  let user: User;
+
+  beforeEach(() => {
+    user = new User();
+    user.salt = 'salt';
+    user.password = 'p4ssw0rd';
+    bcrypt.hash = jest.fn();
+  });
+
+  describe('hasCorrectPassword', () => {
+    it('should return true if password is valid', async () => {
+      bcrypt.hash.mockReturnValue('p4ssw0rd');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+
+      const result = await user.hasCorrectPassword('pass');
+      expect(bcrypt.hash).toHaveBeenCalledWith('pass', 'salt');
+      expect(result).toBeTruthy();
+    });
+
+    it('should return false if password is invalid', async () => {
+      bcrypt.hash.mockReturnValue('n0tTh3C0rr3ctP4ss');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+
+      const result = await user.hasCorrectPassword('pass');
+      expect(bcrypt.hash).toHaveBeenCalledWith('pass', 'salt');
+      expect(result).toBeFalsy();
+    });
+
+  });
+
+});
