575b6926d2be68d96970bd9abb0511bb5db66ea5
Kammradt <vinicius.kammradt1@gmail.com>
2020-05-21 20:31:44 -0300

    :wrench: Adding DB Env vars

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,11 @@ const { type, host, port, username, password, database, synchronize } = config.g
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: type,
-  host: process.env.RDS_HOST || host,
-  port: process.env.RDS_PORT || port,
-  username: process.env.RDS_USERNAME || username,
-  password: process.env.RDS_PASSWORD || password,
-  database: process.env.RDS_DB_NAME || database,
+  host: process.env.DB_HOST || host,
+  port: process.env.DB_PORT || port,
+  username: process.env.DB_USERNAME || username,
+  password: process.env.DB_PASSWORD || password,
+  database: process.env.DB_NAME || database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || synchronize,
 };
