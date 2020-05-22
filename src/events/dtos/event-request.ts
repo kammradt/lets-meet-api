c4c8018bcb87f9dfed0691fcff903fdea4728c14
Kammradt <vinicius.kammradt1@gmail.com>
2020-05-22 01:37:29 -0300

    :sparkles: Adding endpoint to create Events

--- src/events/dtos/event-request.ts
+++ src/events/dtos/event-request.ts
@@ -1,8 +1,26 @@
-import { IsString } from 'class-validator';
+import { IsDateString, IsNumber, IsOptional, IsString, MaxLength, Min, MinLength } from 'class-validator';
 
 export class EventRequest {
 
   @IsString()
-  name: string;
+  @MinLength(8)
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(512)
+  description: string;
+
+  @IsDateString()
+  startDate: Date;
+
+  @IsDateString()
+  endDate: Date;
+
+  @IsNumber()
+  @Min(1)
+  maxAttendees: number;
 
 }
+
+
--- src/events/event.service.ts
+++ src/events/event.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { EventRequest } from './dtos/event-request';
+import { Event } from './event.entity';
+import { User } from '../users/user.entity';
+import { plainToClass } from 'class-transformer';
+import { EventStatus } from './event-status.enum';
+import { UserRole } from '../users/user-role.enum';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor() {
+  }
+
+  public async create(eventRequest: EventRequest, user: User): Promise<Event> {
+    const event = plainToClass(Event, eventRequest);
+    event.owner = user;
+    event.status = EventStatus.OPEN
+
+    this.validate(event, user)
+
+    return await event.save();
+  }
+
+  private validate(event: Event, user: User) {
+    this.validateNumberOfAttendees(event, user);
+  }
+
+  private validateNumberOfAttendees(event: Event, user: User) {
+    if (event.maxAttendees > 50 && user.role == UserRole.REGULAR) {
+      throw new UnauthorizedException('Regular users can only create events with up to 50 attendees');
+      // TODO create custom exception
+    }
+  }
+
+}
